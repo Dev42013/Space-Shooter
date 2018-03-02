6ce70d3d4715f0962a6905f4bc1bc7c432a33a6d
Dev42013 <rickd1977@gmail.com>
2018-03-03 00:36:31 +0900

    Added basic controls for horizontal & vertical movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,14 +5,39 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float yMin = 3f;
+    [Tooltip("In m")] [SerializeField] float yMax = 3f;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
-	}
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawPos = transform.localPosition.x + xOffset;
+
+        float clampedXPos = Mathf.Clamp(rawPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+
+
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedYPos = Mathf.Clamp(rawYPos, -yMin, yMax);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+
+
+    }
 }
